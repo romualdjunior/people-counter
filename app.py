2352b9837a64ec2c2ceaf87409aeea56f0d61bc5
romualdjunior <romuald.motchehokamguia@esprit.tn>
2023-01-22 23:56:38 +0100

    removing app.run

--- app.py
+++ app.py
@@ -442,7 +442,3 @@ def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(count(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-if __name__ == '__main__':
-    app.run()
